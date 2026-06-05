961ac38745e2620721c4f4de85e642510fa69c64
Sanjay kumar c <sanju87921@gmail.com>
2026-06-05 23:18:48 +0530

    chore: remove temp scripts

--- fix.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('c:/Users/sanju/Desktop/Hampi-Stays/frontend/src/pages/admin/AdminDashboard.tsx', 'utf8');
-
-const regex = /import \{ jsPDF \} from "jspdf";\r?\nimport html2canvas from "html2canvas";\r?\n/;
-code = code.replace(regex, '');
-
-fs.writeFileSync('c:/Users/sanju/Desktop/Hampi-Stays/frontend/src/pages/admin/AdminDashboard.tsx', code);
